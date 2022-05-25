0272bf73d54adcb986b4d1cc87117f6ac2f35970
ivansoto <lopezsotoivan01@gmail.com>
2022-05-24 19:30:11 -0500

    first commit

---
+++ src/test/java/com/curame/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curame.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
